5fe5db415b6f3ef1420f233825d4ea305c2dde89
Arhaan <arhaankaushal@outlook.com>
2024-09-25 21:05:51 +0100

    programming heston model

--- Prototyping/Stochastic Modelling/Heston Model Prototyping.py
+++ Prototyping/Stochastic Modelling/Heston Model Prototyping.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+dSt = rStdt + sqrt(Vt)StdW1t
+dVt - k(0 - Vt)dt + osqrt(Vt)dW2t
+
+def Pricing_Differentaial_Equation(num_of_paths,start_price,r,v,time_duration,time_steps):
+    dt = time_duration/time_steps
+    t = np.linspace(0,time_duration, time_steps + 1)
+    wiener_process = 
+    

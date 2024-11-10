7ee49f3eae478fe806f03e5533b0086c5794a9a9
Arhy-hub <arhaankaushal@outlook.com>
2024-11-10 16:04:34 +0000

    need to finish up the get request and sign up to db

--- forecasta web app/website/authentication.py
+++ forecasta web app/website/authentication.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 #user-management api
 authentication = Blueprint('authentication', __name__)
@@ -6,7 +6,7 @@ authentication = Blueprint('authentication', __name__)
 
 #decorator(url) followed by function which produces json or html etc
 #our init file runs the apis
-@authentication.route('/login')
+@authentication.route('/login', methods=['GET','POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -15,6 +15,20 @@ def login():
 def logout():
     return "<h1></h1>"
 
-@authentication.route('/sign-up')
+@authentication.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == "POST":
+        UserName = request.form.get('UserNameInp')
+        Password1 = request.form.get('PasswordInp')
+        Password2 = request.form.get('RepPasswordInp')
+
+        if len(UserName) <= 2:
+            flash('Username is too short. It must be greater than 2 characters', category='error')
+        elif len(Password1) < 8 or len(Password2) < 8:
+            flash('Password length is too short. It must at least be 8 characters', category='error')
+        elif Password1 != Password2:
+            flash('Passwords do not match', category='error')
+        else:
+            #add user to db
+            pass
     return render_template("sign_up.html", boolean=True)

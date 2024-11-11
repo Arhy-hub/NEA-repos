d3f9bdbb9541ff6e56c6bfe02934e38b2141bfe1
Arhy-hub <arhaankaushal@outlook.com>
2024-11-11 22:05:24 +0000

    connecting to db and sending storing in user table

--- forecasta web app/website/authentication.py
+++ forecasta web app/website/authentication.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, flash
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+import re
+import pg8000
 
 #user-management api
 authentication = Blueprint('authentication', __name__)
@@ -9,8 +10,7 @@ authentication = Blueprint('authentication', __name__)
 #our init file runs the apis
 @authentication.route('/login', methods=['GET','POST'])
 def login():
-    data = request.form
-    print(data)
+    print(request.form)
     return render_template("login.html", boolean=True)
 
 
@@ -22,4 +22,30 @@ def logout():
 @authentication.route('/sign-up', methods=['GET','POST'])
 def sign_up():
     print(request.form)
+
+    if request.method == "POST":
+        UserName = request.form.get('UseNameInp')
+        Password = request.form.get('PasswordInp')
+        RepPassword = request.form.get('RepPasswordInp')
+
+        if Password != RepPassword:
+            pass
+        elif len(Password) < 8 or len(RepPassword) < 8:
+            pass
+        elif not re.search("[a-z]" , Password) or not re.search("[a-z]" , RepPassword):
+            pass
+        elif not re.search("[A-Z]" , Password) or not re.search("[A-Z]" , RepPassword):
+            pass
+        elif not re.search("[0-9]" , Password) or not re.search("[0-9]" , RepPassword):
+            pass
+        elif not re.search(r"[!@#$%^&*()\-_+=\\|~`\"'<>.,?{}\[\]:;]" , Password) or not re.search(r"[!@#$%^&*()\-_+=\\|~`\"'<>.,?{}\[\]:;]", RepPassword):
+            pass
+        else:
+            #connect to db
+            connection = pg8000.connect(database="ForecastaDB", user="postgres", password="Password123!", host="localhost", port="5432")
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO users(user_name,password) VALUES(%s,%s);",(UserName,Password))
+            connection.commit() 
+            cursor.close() 
+            connection.close() 
     return render_template("sign_up.html", boolean=True)

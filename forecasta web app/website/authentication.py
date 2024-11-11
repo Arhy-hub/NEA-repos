67ddb841f0a49d43b531959bceb128169dc3c1df
Arhy-hub <arhaankaushal@outlook.com>
2024-11-11 19:18:43 +0000

    POST REQUEST WORKING

--- forecasta web app/website/authentication.py
+++ forecasta web app/website/authentication.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 
+
 #user-management api
 authentication = Blueprint('authentication', __name__)
 #blueprint to class urls
@@ -8,27 +9,17 @@ authentication = Blueprint('authentication', __name__)
 #our init file runs the apis
 @authentication.route('/login', methods=['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True)
 
 
-@authentication.route('/logout')
+@authentication.route('/logout', methods=['GET','POST'])
 def logout():
-    return "<h1></h1>"
+    print(request.form)
+    return render_template("base.html", boolean=True)
 
 @authentication.route('/sign-up', methods=['GET','POST'])
 def sign_up():
-    if request.method == "POST":
-        UserName = request.form.get('UserNameInp')
-        Password1 = request.form.get('PasswordInp')
-        Password2 = request.form.get('RepPasswordInp')
-
-        if len(UserName) <= 2:
-            flash('Username is too short. It must be greater than 2 characters', category='error')
-        elif len(Password1) < 8 or len(Password2) < 8:
-            flash('Password length is too short. It must at least be 8 characters', category='error')
-        elif Password1 != Password2:
-            flash('Passwords do not match', category='error')
-        else:
-            #add user to db
-            pass
+    print(request.form)
     return render_template("sign_up.html", boolean=True)
